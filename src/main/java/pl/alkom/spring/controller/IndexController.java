b58a5e7aa355bc4ee02dfba312f161e209501803
bakeraw2 <michalskidaniel2@gmail.com>
2014-08-31 13:23:50 +0200

    Delete non-usefull code

--- src/main/java/pl/alkom/spring/controller/IndexController.java
+++ src/main/java/pl/alkom/spring/controller/IndexController.java
@@ -1,12 +1,9 @@
 package pl.alkom.spring.controller;
 
 import org.slf4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import pl.alkom.spring.model.Person;
 import pl.alkom.spring.util.log.AutowiredLogger;
 
 @Controller
@@ -18,10 +15,6 @@ public class IndexController {
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome() {
-
-
-        logger.info("DZIAŁA");
-
         return "index";
 	}
 }
